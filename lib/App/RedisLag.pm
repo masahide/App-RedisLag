1703457c09281d78667a6910b4465bade9fa0cb7
masahide <masahide.y@gmail.com>
2013-07-01 09:36:59 +0900

    initial commit

---
+++ lib/App/RedisLag.pm
@@ -0,0 +1,32 @@
+package App::RedisLag;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+App::RedisLag -
+
+=head1 SYNOPSIS
+
+  use App::RedisLag;
+
+=head1 DESCRIPTION
+
+App::RedisLag is
+
+=head1 AUTHOR
+
+YAMASAKI Masahide E<lt>masahide.y@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
