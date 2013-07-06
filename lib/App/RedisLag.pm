038cc435bbbc25fff5f133af456d29984a3d7ac6
masahide <masahide.y@gmail.com>
2013-07-06 22:22:02 +0900

    retab

--- lib/App/RedisLag.pm
+++ lib/App/RedisLag.pm
@@ -11,163 +11,163 @@ use App::RedisLag::Redis;
 use constant KEY_PREFIX =>"RedisLag";
 
 sub new{
-	my $class = shift;
+    my $class = shift;
 
-	my $self = bless {
-		key			=> "ae9974b9-0ff5-4f7c-a572-4442fa0ef2d5-". $$,
-		lag			=> 0,
-		timeout		=> 120,
-		wait		=> 0.020,
-		verbose		=> 0,
-		debug		=> 0,
-		daemon		=> 0,
-		set_time	=> undef,
-		master		=> undef,
-		slave		=> undef,
-		slave_host	=> 'localhost',
-		slave_port	=> 6379,
-		result_key  => 'slave',
-		master_host	=> 'localhost',
-		master_port	=> 6379,
-		ring_buf	=> [],
-		@_,
-	}, $class;
-	$self->{result_key}	= KEY_PREFIX."-".$self->{result_key};
-	$self->{slave}  = App::RedisLag::Redis->new(host=>$self->{slave_host},port=>$self->{slave_port});
-	$self->{master} = App::RedisLag::Redis->new(host=>$self->{master_host},port=>$self->{master_port});
-	$self->{verbose} = 1 if $self->{debug};
-	openlog("ReidsLag",'cons,pid', 'local5') if $self->{daemon};
-	
-	$self;
+    my $self = bless {
+        key         => "ae9974b9-0ff5-4f7c-a572-4442fa0ef2d5-". $$,
+        lag         => 0,
+        timeout     => 120,
+        wait        => 0.020,
+        verbose     => 0,
+        debug       => 0,
+        daemon      => 0,
+        set_time    => undef,
+        master      => undef,
+        slave       => undef,
+        slave_host  => 'localhost',
+        slave_port  => 6379,
+        result_key  => 'slave',
+        master_host => 'localhost',
+        master_port => 6379,
+        ring_buf    => [],
+        @_,
+    }, $class;
+    $self->{result_key} = KEY_PREFIX."-".$self->{result_key};
+    $self->{slave}  = App::RedisLag::Redis->new(host=>$self->{slave_host},port=>$self->{slave_port});
+    $self->{master} = App::RedisLag::Redis->new(host=>$self->{master_host},port=>$self->{master_port});
+    $self->{verbose} = 1 if $self->{debug};
+    openlog("ReidsLag",'cons,pid', 'local5') if $self->{daemon};
+    
+    $self;
 }
 
 #アクセサ
 sub key {
-	my $self = shift ;
-	return ( $_[0] )?  $self->{key} = $_[0] : $self->{key};
+    my $self = shift ;
+    return ( $_[0] )?  $self->{key} = $_[0] : $self->{key};
 }
 sub lag {
-	my $self = shift ;
-	return ( $_[0] )?  $self->{lag} = $_[0] : $self->{lag};
+    my $self = shift ;
+    return ( $_[0] )?  $self->{lag} = $_[0] : $self->{lag};
 }
 sub timeout {
-	my $self = shift ;
-	return ( $_[0] )?  $self->{timeout} = $_[0] : $self->{timeout};
+    my $self = shift ;
+    return ( $_[0] )?  $self->{timeout} = $_[0] : $self->{timeout};
 }
 sub wait {
-	my $self = shift ;
-	return ( $_[0] )?  $self->{wait} = $_[0] : $self->{wait};
+    my $self = shift ;
+    return ( $_[0] )?  $self->{wait} = $_[0] : $self->{wait};
 }
 sub set_time {
-	my $self = shift ;
-	return ( $_[0] )?  $self->{set_time} = $_[0] : $self->{set_time};
+    my $self = shift ;
+    return ( $_[0] )?  $self->{set_time} = $_[0] : $self->{set_time};
 }
 sub add_ring_buf {
-	my ($self,$value) = @_;
-	push(@{$self->{ring_buf}},$value);
-	if($#{$self->{ring_buf}} >= 600){
-		shift(@{$self->{ring_buf}});
-	}
+    my ($self,$value) = @_;
+    push(@{$self->{ring_buf}},$value);
+    if($#{$self->{ring_buf}} >= 600){
+        shift(@{$self->{ring_buf}});
+    }
 }
 
 sub sum {
-	my ($self) = @_;
-	my $time = Time::HiRes::time - 330;
-	my $counter = 0;
-	my $sum = 0;
-	my $max = 0;
-	my $min = $self->{timeout};
-	foreach my $data (@{$self->{ring_buf}}){
-		if($data->{date} > $time){
-			$sum += $data->{time};
-			$max = $data->{time} if($max<$data->{time});
-			$min = $data->{time} if($min>$data->{time});
-			$counter++;
-		}
-	}
-	my $avg = $sum / $counter;
-	return {avg=>$avg,max=>,$max,min=>$min,count=>$counter};
+    my ($self) = @_;
+    my $time = Time::HiRes::time - 330;
+    my $counter = 0;
+    my $sum = 0;
+    my $max = 0;
+    my $min = $self->{timeout};
+    foreach my $data (@{$self->{ring_buf}}){
+        if($data->{date} > $time){
+            $sum += $data->{time};
+            $max = $data->{time} if($max<$data->{time});
+            $min = $data->{time} if($min>$data->{time});
+            $counter++;
+        }
+    }
+    my $avg = $sum / $counter;
+    return {avg=>$avg,max=>,$max,min=>$min,count=>$counter};
 }
 
 
 sub run {
-	my ($self) = @_;
-	my $time = $self->set_check_value();
-	$self->puts('debug', "set check value: $time") if $self->{debug};
-	if(!$time){
-		return $time;
-	}
-	$time  = $self->get_check_value($time);
-	$self->puts('debug', "get check value: $time" ) if $self->{debug};
-	if(!$time){
-		return $time
-	}
-	$self->add_ring_buf({date=>time(),time=>$time});
-	my $sum = $self->sum();
-	my $value = "current:$time\tmin:$sum->{min}\tmax:$sum->{max}\tavg:$sum->{avg}";
-	$self->{master}->set_value( $self->{result_key}, $value);
-	$self->puts('debug', "set result_key:$self->{result_key} value: $value") if $self->{debug};
-	if($self->{verbose}){
-		my $message = "current=>$time";
-		map{ $message .= ", $_=>$sum->{$_}" } keys %{$sum};
-		$self->puts('debug', $message);
-	}
+    my ($self) = @_;
+    my $time = $self->set_check_value();
+    $self->puts('debug', "set check value: $time") if $self->{debug};
+    if(!$time){
+        return $time;
+    }
+    $time  = $self->get_check_value($time);
+    $self->puts('debug', "get check value: $time" ) if $self->{debug};
+    if(!$time){
+        return $time
+    }
+    $self->add_ring_buf({date=>time(),time=>$time});
+    my $sum = $self->sum();
+    my $value = "current:$time\tmin:$sum->{min}\tmax:$sum->{max}\tavg:$sum->{avg}";
+    $self->{master}->set_value( $self->{result_key}, $value);
+    $self->puts('debug', "set result_key:$self->{result_key} value: $value") if $self->{debug};
+    if($self->{verbose}){
+        my $message = "current=>$time";
+        map{ $message .= ", $_=>$sum->{$_}" } keys %{$sum};
+        $self->puts('debug', $message);
+    }
 }
 
 sub get_result {
-	my ($self) = @_;
-	my $line = $self->{master}->get_value($self->{result_key});
-	$self->puts('debug', "get result_key:$self->{result_key} value: $line") if $self->{debug};
-	if($line =~ /\t/){
-		my %kv;
-		for (map { [ split ':', $_, 2 ] } split "\t", $line) {
-			$kv{$_->[0]} = $_->[1];
-		}
-		return \%kv;
-	}
-	else{
-		return 0;
-	}
+    my ($self) = @_;
+    my $line = $self->{master}->get_value($self->{result_key});
+    $self->puts('debug', "get result_key:$self->{result_key} value: $line") if $self->{debug};
+    if($line =~ /\t/){
+        my %kv;
+        for (map { [ split ':', $_, 2 ] } split "\t", $line) {
+            $kv{$_->[0]} = $_->[1];
+        }
+        return \%kv;
+    }
+    else{
+        return 0;
+    }
 }
 sub set_check_value {
-	my ($self) = @_;
-	$self->{set_time} = Time::HiRes::time;
-	if($self->{master}->set_value($self->{key},$self->{set_time})){
-		return $self->{set_time};
-	}
-	return 0;
+    my ($self) = @_;
+    $self->{set_time} = Time::HiRes::time;
+    if($self->{master}->set_value($self->{key},$self->{set_time})){
+        return $self->{set_time};
+    }
+    return 0;
 }
 sub get_check_value {
-	my ($self, $set_time) = @_;
-	if($set_time){ $self->{set_time} = $set_time; }
-	$set_time = $self->{set_time};
-	if(!$set_time){
-		return 0;
-	}
-	while(1){
-		my $time = Time::HiRes::time;
-		if($self->{timeout} < ($time - $set_time)){
-			return $self->{timeout};
-		}
-		my $value = $self->{slave}->get_value($self->{key});
-		if(!$value){
-			return $value;
-		}
-		elsif($set_time eq $value){
-			return $time - $set_time;
-		}
-		Time::HiRes::sleep($self->{wait});
-	}
+    my ($self, $set_time) = @_;
+    if($set_time){ $self->{set_time} = $set_time; }
+    $set_time = $self->{set_time};
+    if(!$set_time){
+        return 0;
+    }
+    while(1){
+        my $time = Time::HiRes::time;
+        if($self->{timeout} < ($time - $set_time)){
+            return $self->{timeout};
+        }
+        my $value = $self->{slave}->get_value($self->{key});
+        if(!$value){
+            return $value;
+        }
+        elsif($set_time eq $value){
+            return $time - $set_time;
+        }
+        Time::HiRes::sleep($self->{wait});
+    }
 }
 
 sub puts {
-	my ($self, $type,$message) = @_;
-	if($self->{daemon}){
-		syslog($type,$message);
-	}
-	else{
-		print $message."\n";
-	}
+    my ($self, $type,$message) = @_;
+    if($self->{daemon}){
+        syslog($type,$message);
+    }
+    else{
+        print $message."\n";
+    }
 
 }
 
--- lib/App/RedisLag/Redis.pm
+++ lib/App/RedisLag/Redis.pm
@@ -8,129 +8,129 @@ use IO::Select;
 use constant EXPIRE => 600;
 
 sub new{
-	my $class = shift;
+    my $class = shift;
 
-	my $self = bless {
-		socket		=> undef,
-		host		=> "localhost",
-		port		=> 6379,
-		pass		=> undef,
-		server		=> undef,
-		error_msg	=> "",
-		@_,
-	}, $class;
-	$self;
+    my $self = bless {
+        socket      => undef,
+        host        => "localhost",
+        port        => 6379,
+        pass        => undef,
+        server      => undef,
+        error_msg   => "",
+        @_,
+    }, $class;
+    $self;
 }
 
 
 #アクセサ
 sub error_msg{
-	my $self = shift ;
-	return ( $_[0] )?  $self->{error_msg} = $_[0] : $self->{error_msg};
+    my $self = shift ;
+    return ( $_[0] )?  $self->{error_msg} = $_[0] : $self->{error_msg};
 }
 sub error {
-	my ($self, $msg, $err_no) = @_;
-	$self->{error_msg} = "$msg : $err_no";
-	return 0;
+    my ($self, $msg, $err_no) = @_;
+    $self->{error_msg} = "$msg : $err_no";
+    return 0;
 }
 
 sub connect {
-	my ($self, $host, $port, $pass) = @_;
-	if($self->{socket}){
-		if($self->{socket}->connected){
-			return $self; # 接続済み
-		}
-	}
-	if($host){ $self->{host} = $host; }
-	if($port){ $self->{port} = $port; }
-	if($pass){ $self->{port} = $pass; }
-	my $server = $self->{host}.":".$self->{port};
-	$self->{server}=$server;
-	my $sock = IO::Socket::INET->new(
-		PeerAddr=>$server,
-		TimeOut=>5,
-		Blocking=>0,
-		Proto=>'tcp');
-	if($sock){
-		if ($pass) {
-			$sock->print("AUTH $pass \r\n");
-			<$sock> || return $self->error("[$server] socket auth error",$!);
-		}
-	}
-	else{
-		return $self->error("socket connect error: $!");
-	}
-	$self->{socket} = $sock;
+    my ($self, $host, $port, $pass) = @_;
+    if($self->{socket}){
+        if($self->{socket}->connected){
+            return $self; # 接続済み
+        }
+    }
+    if($host){ $self->{host} = $host; }
+    if($port){ $self->{port} = $port; }
+    if($pass){ $self->{port} = $pass; }
+    my $server = $self->{host}.":".$self->{port};
+    $self->{server}=$server;
+    my $sock = IO::Socket::INET->new(
+        PeerAddr=>$server,
+        TimeOut=>5,
+        Blocking=>0,
+        Proto=>'tcp');
+    if($sock){
+        if ($pass) {
+            $sock->print("AUTH $pass \r\n");
+            <$sock> || return $self->error("[$server] socket auth error",$!);
+        }
+    }
+    else{
+        return $self->error("socket connect error: $!");
+    }
+    $self->{socket} = $sock;
 
-	return $self;
+    return $self;
 }
 
 sub close {
-	my ($self) = @_;
-	my $s = $self->{socket};
-	if($s){
-		if($s->connected){
-			return $s->close();
-		}
-	}
-	return $s;
+    my ($self) = @_;
+    my $s = $self->{socket};
+    if($s){
+        if($s->connected){
+            return $s->close();
+        }
+    }
+    return $s;
 }
 
 sub DESTROY {
-	my ($self) = @_;
-	$self->close();
+    my ($self) = @_;
+    $self->close();
 }
 
 sub set_value {
-	my ($self,$key,$value) = @_;
-	my $status;
-	my @lines = $self->write_read("SET $key $value\r\nEXPIRE $key ".EXPIRE."\r\n");
-	if(!$#lines){ return 0;}
-	if($#lines != 1 ||
-	   ($lines[0] !~ /^\+OK/ && $lines[1] !~ /^\:1/)
-	){
-		return $self->error("[".$self->{server}."] get($key) socket read error", join(",",@lines));
-	}
-	return $self;
+    my ($self,$key,$value) = @_;
+    my $status;
+    my @lines = $self->write_read("SET $key $value\r\nEXPIRE $key ".EXPIRE."\r\n");
+    if(!$#lines){ return 0;}
+    if($#lines != 1 ||
+       ($lines[0] !~ /^\+OK/ && $lines[1] !~ /^\:1/)
+    ){
+        return $self->error("[".$self->{server}."] get($key) socket read error", join(",",@lines));
+    }
+    return $self;
 }
 
 sub get_value {
-	my ($self,$key) = @_;
-	$self->connect();
-	my @lines = $self->write_read("GET $key\r\n");
-	if(!$#lines){ return 0;}
-	if($#lines != 1){
-		return $self->error("[".$self->{server}."] get($key) socket read error", -1);
-	}
-	return $lines[1];
+    my ($self,$key) = @_;
+    $self->connect();
+    my @lines = $self->write_read("GET $key\r\n");
+    if(!$#lines){ return 0;}
+    if($#lines != 1){
+        return $self->error("[".$self->{server}."] get($key) socket read error", -1);
+    }
+    return $lines[1];
 }
 
 
 sub write_read {
-	my ($self,$in) = @_;
-	$self->connect();
-	my $s = $self->{socket};
-	my $server = $self->{server};
-	my $selecter = IO::Select->new;
-	$selecter->add($s);
-	$s->print($in);
-	$s->flush();
-	my $buf;
-	my @lines;
-	my @ready = $selecter->can_read(5);
-	if(@ready){
-		my $len = read($s, $buf, 4096);
-		$selecter->remove($s);
-		@lines = split(/\r\n/,$buf);
-	}
-	else{
-		@lines = ();
-	}
-	$selecter->remove($s);
-	return @lines;
+    my ($self,$in) = @_;
+    $self->connect();
+    my $s = $self->{socket};
+    my $server = $self->{server};
+    my $selecter = IO::Select->new;
+    $selecter->add($s);
+    $s->print($in);
+    $s->flush();
+    my $buf;
+    my @lines;
+    my @ready = $selecter->can_read(5);
+    if(@ready){
+        my $len = read($s, $buf, 4096);
+        $selecter->remove($s);
+        @lines = split(/\r\n/,$buf);
+    }
+    else{
+        @lines = ();
+    }
+    $selecter->remove($s);
+    return @lines;
 }
-	
-	
+    
+    
 
 
 1;
